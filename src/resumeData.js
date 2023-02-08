eb4c13ceacdd0ba3ab14577f74111a26056d3428
Mtomchik <matthew.tomchik@yahoo.com>
2023-02-07 20:32:06 -0700

    starter code

---
+++ src/resumeData.js
@@ -0,0 +1,99 @@
+let resumeData = {
+    "imagebaseurl":"https://mtomchik.github.io/React-Portfolio/",
+    "name": "Matt Tomchik",
+    "role": "Fullstack Developer",
+    "linkedinId":"https://www.linkedin.com/in/matt-tomchik/",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/matt-tomchik/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/mtomchik",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently a student at he University of Denver coding bootcamp where I am learning fullstack web development in a fast paced, self driven enviornment. I have previous experience in healthcare as a Occupational Therapy Assistant.",
+    "address":"Denver, CO",
+    "website":"https://mtomchik.github.io/React-Portfolio/",
+    "education":[
+      {
+        "UniversityName":"DU coding bootcamp",
+        "specialization":"Fullstack web development",
+        "MonthOfPassing":"Feb",
+        "YearOfPassing":"2023",
+      },
+      {
+        "UniversityName":"California State University Long Beach",
+        "specialization":"English Education",
+        "MonthOfPassing":"Jun",
+        "YearOfPassing":"2011",
+        "Achievements":"BA"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Accelerate Lakewood",
+        "specialization":"Occupational Therapy Assistant",
+        "MonthOfLeaving":"Currently Employed",
+        "YearOfLeaving":"Currently Employed",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"The Center at Lowry",
+        "specialization":"Occupational Therapy Assistant",
+        "MonthOfLeaving":"Dec",
+        "YearOfLeaving":"2019",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
