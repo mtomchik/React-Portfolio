406ce7f90eb043cd0645823bad3ecdd9eb973747
Mtomchik <matthew.tomchik@yahoo.com>
2023-02-15 14:17:59 -0700

    Updates

---
+++ precache-manifest.89c9068eaefb601e2ac59457a7036e8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ff2ed3e01d7eb1c33ab",
+    "url": "/React-Portfolio/static/css/main.890db19a.chunk.css"
+  },
+  {
+    "revision": "0ff2ed3e01d7eb1c33ab",
+    "url": "/React-Portfolio/static/js/main.0ff2ed3e.chunk.js"
+  },
+  {
+    "revision": "02feadb2adb76b10ee9b",
+    "url": "/React-Portfolio/static/js/1.02feadb2.chunk.js"
+  },
+  {
+    "revision": "804d233a10add83efa8d",
+    "url": "/React-Portfolio/static/js/runtime~main.804d233a.js"
+  },
+  {
+    "revision": "0a8bc0529953cead74f1282146ff9127",
+    "url": "/React-Portfolio/index.html"
+  }
+];
